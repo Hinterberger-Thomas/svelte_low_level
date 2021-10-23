f9d190645e88f8900e2ca7c4cc438c01fcdafe9f
Hinterberger-Thomas <thomas.hinterberger.eu@gmail.com>
2021-10-23 17:17:11 +0200

    init svelte

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import * as api from "./util/api" 
+	api.ledOn()
+</script>
+
+<main>
+	<h1>this is just a test </h1>
+	<button on:click = {() => console.log("hello")}></button>
+</main>
+
+<style>
+
+</style>
